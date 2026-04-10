15066a9223ddd808da5cd710f5ac23ff0ce15ef9
Lucas Santana <lucas.diassantana@gmail.com>
2026-04-10 14:12:56 -0300

    refactor: nowplaying delegates to songinfo execute to eliminate duplication

--- packages/bot/src/functions/music/commands/nowplaying.ts
+++ packages/bot/src/functions/music/commands/nowplaying.ts
@@ -1,35 +1,11 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import Command from '../../../models/Command'
-import { interactionReply } from '../../../utils/general/interactionReply'
-import { buildTrackEmbed, trackToData } from '../../../utils/general/responseEmbeds'
-import type { CommandExecuteParams } from '../../../types/CommandData'
-import {
-    requireQueue,
-    requireCurrentTrack,
-} from '../../../utils/command/commandValidations'
-import { resolveGuildQueue } from '../../../utils/music/queueResolver'
+import songinfoCommand from './songinfo'
 
 export default new Command({
     data: new SlashCommandBuilder()
         .setName('nowplaying')
         .setDescription('🎵 Show what\'s currently playing'),
     category: 'music',
-    execute: async ({ client, interaction }: CommandExecuteParams) => {
-        const { queue } = resolveGuildQueue(client, interaction.guildId ?? '')
-        const track = queue?.currentTrack
-
-        if (!(await requireQueue(queue, interaction))) return
-        if (!(await requireCurrentTrack(queue, interaction))) return
-
-        const trackData = trackToData(track!)
-        const embed = buildTrackEmbed(trackData, 'playing', {
-            tag: interaction.user.username,
-            displayAvatarURL: interaction.user.displayAvatarURL,
-        })
-
-        await interactionReply({
-            interaction,
-            content: { embeds: [embed] },
-        })
-    },
+    execute: songinfoCommand.execute,
 })
