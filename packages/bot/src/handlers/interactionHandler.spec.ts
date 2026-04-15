802b883a70a7d1da362258f6d4de94350ef964b7
Lucas Santana <lucas.diassantana@gmail.com>
2026-04-15 00:20:21 -0300

    fix(bot): update test imports for errorSanitizer subpath

--- packages/bot/src/handlers/interactionHandler.spec.ts
+++ packages/bot/src/handlers/interactionHandler.spec.ts
@@ -51,13 +51,13 @@ jest.mock('../utils/monitoring', () => ({
 }))
 
 import { debugLog, errorLog } from '@lucky/shared/utils'
+import { createUserFriendlyError } from '@lucky/shared/utils/general/errorSanitizer'
 import { executeCommand } from './commandsHandler'
 import { handleMusicButtonInteraction } from './musicButtonHandler'
 import { reactionRolesService } from '@lucky/shared/services'
 import { errorEmbed } from '../utils/general/embeds'
 import { interactionReply } from '../utils/general/interactionReply'
 import { monitorInteractionHandling } from '../utils/monitoring'
-import { createUserFriendlyError } from '@lucky/shared/utils'
 
 function createMockClient(): CustomClient & {
     eventHandlers: Map<string, Function[]>
