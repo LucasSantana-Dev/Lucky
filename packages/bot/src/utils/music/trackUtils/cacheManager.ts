c447359bd6dace6d7260d406e5377ea22222e4d3
Lucas Santana <lucas.diassantana@gmail.com>
2026-06-12 10:36:17 -0300

    refactor: consolidate caches to lru-cache (#1291)

--- packages/bot/src/utils/music/trackUtils/cacheManager.ts
+++ packages/bot/src/utils/music/trackUtils/cacheManager.ts
@@ -1,55 +1,5 @@
+import { LRUCache } from 'lru-cache'
 import type { TrackCacheKey, TrackInfo, TrackCacheOptions } from './types'
-
-/**
- * LRU cache for track info
- */
-export class LRUCache<K, V> {
-    private readonly cache = new Map<K, V>()
-    private readonly maxSize: number
-
-    constructor(maxSize: number) {
-        this.maxSize = maxSize
-    }
-
-    get(key: K): V | undefined {
-        const value = this.cache.get(key)
-        if (value !== undefined) {
-            // Move to end (most recently used)
-            this.cache.delete(key)
-            this.cache.set(key, value)
-        }
-        return value
-    }
-
-    set(key: K, value: V): void {
-        if (this.cache.has(key)) {
-            this.cache.delete(key)
-        } else if (this.cache.size >= this.maxSize) {
-            // Remove least recently used (first item)
-            const firstKey = this.cache.keys().next().value
-            if (firstKey !== undefined) {
-                this.cache.delete(firstKey)
-            }
-        }
-        this.cache.set(key, value)
-    }
-
-    has(key: K): boolean {
-        return this.cache.has(key)
-    }
-
-    delete(key: K): boolean {
-        return this.cache.delete(key)
-    }
-
-    clear(): void {
-        this.cache.clear()
-    }
-
-    size(): number {
-        return this.cache.size
-    }
-}
 
 /**
  * Track cache manager
@@ -60,8 +10,7 @@ export class TrackCacheManager {
 
     constructor(options: TrackCacheOptions = { maxSize: 1000, ttl: 300000 }) {
         this.options = options
-        this.cache = new LRUCache(options.maxSize)
-        // options is used for initialization above
+        this.cache = new LRUCache({ max: options.maxSize, ttl: options.ttl })
     }
 
     get(key: TrackCacheKey): TrackInfo | undefined {
@@ -89,7 +38,7 @@ export class TrackCacheManager {
     }
 
     size(): number {
-        return this.cache.size()
+        return this.cache.size
     }
 
     getOptions(): TrackCacheOptions {
